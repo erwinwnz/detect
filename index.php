b67a121b7d8f9d239ae3ec8df7aaf044da5706b0
Erwin <erwinwnz@gmail.com>
2018-05-18 00:24:35 +0700

    update

--- index.php
+++ index.php
@@ -2,6 +2,85 @@
 
 require_once('./PhpUserAgent-master/Source/UserAgentParser.php');
 
+function getIP($ip = NULL,  $deep_detect = TRUE) {
+	if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+        $ip = $_SERVER["REMOTE_ADDR"];
+        if ($deep_detect) {
+            if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+    }
+    return $ip;
+}
+
+function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
+    $output = NULL;
+    if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+        $ip = $_SERVER["REMOTE_ADDR"];
+        if ($deep_detect) {
+            if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+    }
+    $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+    $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+    $continents = array(
+        "AF" => "Africa",
+        "AN" => "Antarctica",
+        "AS" => "Asia",
+        "EU" => "Europe",
+        "OC" => "Australia (Oceania)",
+        "NA" => "North America",
+        "SA" => "South America"
+    );
+    if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+        $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+        if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+            switch ($purpose) {
+                case "location":
+                    $output = array(
+                        "city"           => @$ipdat->geoplugin_city,
+                        "state"          => @$ipdat->geoplugin_regionName,
+                        "country"        => @$ipdat->geoplugin_countryName,
+                        "country_code"   => @$ipdat->geoplugin_countryCode,
+                        "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                        "continent_code" => @$ipdat->geoplugin_continentCode
+                    );
+                    break;
+                case "address":
+                    $address = array($ipdat->geoplugin_countryName);
+                    if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                        $address[] = $ipdat->geoplugin_regionName;
+                    if (@strlen($ipdat->geoplugin_city) >= 1)
+                        $address[] = $ipdat->geoplugin_city;
+                    $output = implode(", ", array_reverse($address));
+                    break;
+                case "city":
+                    $output = @$ipdat->geoplugin_city;
+                    break;
+                case "state":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "region":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "country":
+                    $output = @$ipdat->geoplugin_countryName;
+                    break;
+                case "countrycode":
+                    $output = @$ipdat->geoplugin_countryCode;
+                    break;
+            }
+        }
+    }
+    return $output;
+}
+ 
+
 function getClientIPInfo($ip = null) {
 	global $_SERVER;
 		
@@ -13,19 +92,14 @@ function getClientIPInfo($ip = null) {
     return $data;
 }
 
-$query = @unserialize (file_get_contents('http://ip-api.com/php/'));
-if ($query && $query['status'] == 'success') {
-echo 'Hey user from ' . $query['country'] . ', ' . $query['city'] . '!';
-}
-foreach ($query as $data) {
-    echo $data . "<br>";
-}
 $geoplugin = getClientIPInfo();
 $ua_info = parse_user_agent();
-
-echo "Geolocation results for {$_SERVER['REMOTE_ADDR']}: <br />\n".
-	"City: {$geoplugin->city} <br />\n".
-	"Country Name: {$geoplugin->country_name} <br />\n".
+$ip = getIP();
+$country = ip_info("visitor", "Country");
+$city = ip_info("visitor", "City");
+echo "Geolocation results for {$ip}: <br />\n".
+	"City: {$city} <br />\n".
+	"Country Name: {$country} <br />\n".
 	"Platform: {$ua_info['platform']} <br />\n".
 	"Browser: {$ua_info['browser']} <br />\n".
 	"Version: {$ua_info['version']} <br />\n";
